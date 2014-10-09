00891132e4bdfe96a87a2c9f0c5d1fde93a2d120
Alexandre Viau <reazem@reazem.net>
2014-10-08 20:53:29 -0400

    Update travis-install.sh

--- toxin-client/travis-install.sh
+++ toxin-client/travis-install.sh
@@ -3,5 +3,4 @@ sh -e /etc/init.d/xvfb start
 npm start > /dev/null &
 npm run update-webdriver
 sleep 1 # give server time to start
-node_modules/.bin/karma start karma.conf.js --no-auto-watch --single-run --reporters=dots --browsers=Firefox
-node_modules/.bin/protractor e2e-tests/protractor.conf.js --browser=firefox
+npm run protractor
